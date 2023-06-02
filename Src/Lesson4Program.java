b49403008df5f1958e7bf42d51ca412ca9487f45
AndreyKhrypach <pacik78@gmail.com>
2023-06-02 20:49:02 +0300

    another message

--- Src/Lesson4Program.java
+++ Src/Lesson4Program.java
@@ -2,5 +2,6 @@ public class Lesson4Program {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git from Andrey");
+        System.out.println("i am here");
     }
 }
